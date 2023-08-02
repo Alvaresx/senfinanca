bff8a208ced6c7ccf47e1675c3ae383ee1f32760
Mariana Alvares <mariana11areal@hotmail.com>
2023-08-02 15:55:08 -0300

    ajustando rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import DashboardView from "../views/Dashboard";
 import TransactionsTableView from "../views/TransactionsTable";
-import CreateTransactionsView from "../views/CreateTransactions";
+import CreateTransactionsContainer from "../containers/CreateTransactions";
 
 const routes = () => (
   <Routes>
     <Route element={<DashboardView />} path="/"></Route>
-    <Route element={<CreateTransactionsView />} path="registro"></Route>
+    <Route element={<CreateTransactionsContainer />} path="registro"></Route>
     <Route element={<TransactionsTableView />} path="transacoes"></Route>
   </Routes>
 );
